1d68b124da3aae876fd171882a18688e3e02f0fc
Kristian Alvarez Jörgensen <kristian.alvarez.jorgensen@scania.com>
2024-10-16 14:54:35 +0200

    Make page fully static

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static('src/static'));
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-});
